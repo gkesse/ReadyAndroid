b045768be62580a8e99ee2e114e1b4d5146cd7f7
Gerard KESSE <tiakagerard@hotmail.com>
2020-09-25 06:36:12 +0200

    Initial Commit

--- code/app/src/main/java/com/pkg/readyapp/GSQLiteMgr.java
+++ code/app/src/main/java/com/pkg/readyapp/GSQLiteMgr.java
@@ -2,6 +2,7 @@
 package com.pkg.readyapp;
 //===============================================
 import android.content.*;
+import android.widget.*;
 import android.database.*;
 import android.database.sqlite.*;
 //===============================================
@@ -26,7 +27,7 @@ public class GSQLiteMgr {
         lQuery = ""+
         "drop table CONFIG_ANDROID\n"+
         "";
-        queryCreate(lQuery);
+        queryWrite(lQuery);
         // creation
         lQuery = ""+
         "create table CONFIG_ANDROID(\n"+
@@ -34,16 +35,75 @@ public class GSQLiteMgr {
         "CONFIG_VALUE text\n"+
         ")\n"+
         "";
-        queryCreate(lQuery);
+        queryWrite(lQuery);
+        // creation
+        lQuery = ""+
+        "insert into CONFIG_ANDROID(CONFIG_KEY, CONFIG_VALUE)\n"+
+        "values('MY_KEY_01', 'MY_VALUE_01')\n"+
+        "";
+        queryWrite(lQuery);
+        lQuery = ""+
+        "insert into CONFIG_ANDROID(CONFIG_KEY, CONFIG_VALUE)\n"+
+        "values('MY_KEY_02', 'MY_VALUE_02')\n"+
+        "";
+        queryWrite(lQuery);
+        lQuery = ""+
+        "insert into CONFIG_ANDROID(CONFIG_KEY, CONFIG_VALUE)\n"+
+        "values('MY_KEY_03', 'MY_VALUE_03')\n"+
+        "";
+        queryWrite(lQuery);
+        // affichage
+        lQuery = ""+
+        "select * from CONFIG_ANDROID\n"+
+        "";
+        queryShow(lQuery);
     }
     //===============================================
-    public void queryCreate(String sql) {        
+    public void queryWrite(String sql) {        
         sGAndroid lAndroid = GManager.Instance().dataGet().android;
         sGSQLite lSqlite = GManager.Instance().dataGet().sqlite;
         GSQLiteHelper lDbHelper = new GSQLiteHelper(lAndroid.context, lSqlite.db_name, lSqlite.db_version);
         SQLiteDatabase lDb = lDbHelper.getWritableDatabase();
         lDb.execSQL(sql);
         lDb.close();
+    }
+    //===============================================
+    public void queryShow(String sql) {        
+        sGAndroid lAndroid = GManager.Instance().dataGet().android;
+        sGSQLite lSqlite = GManager.Instance().dataGet().sqlite;
+        GSQLiteHelper lDbHelper = new GSQLiteHelper(lAndroid.context, lSqlite.db_name, lSqlite.db_version);
+        SQLiteDatabase lDb = lDbHelper.getWritableDatabase();
+        Cursor lCursor = lDb.rawQuery(sql, null);
+        String lValue = "";
+        if(lCursor.moveToFirst()) {
+            int lColCount = lCursor.getColumnCount();
+            while(true) {
+                for(int i = 0; i < lColCount; i++) {
+                    if(i != 0) lValue += " | ";
+                    lValue += lCursor.getString(i);
+                }
+                lValue += "\n";
+                if(!lCursor.moveToNext()) break;
+            }
+        }
+        Toast.makeText(lAndroid.context, lValue, Toast.LENGTH_SHORT).show();
+        lCursor.close();
+        lDb.close();
+    }
+    //===============================================
+    public String queryValue(String sql) {        
+        sGAndroid lAndroid = GManager.Instance().dataGet().android;
+        sGSQLite lSqlite = GManager.Instance().dataGet().sqlite;
+        GSQLiteHelper lDbHelper = new GSQLiteHelper(lAndroid.context, lSqlite.db_name, lSqlite.db_version);
+        SQLiteDatabase lDb = lDbHelper.getWritableDatabase();
+        Cursor lCursor = lDb.rawQuery(sql, null);
+        String lValue = "";
+        if(lCursor.moveToFirst()) {
+            lValue = lCursor.getString(0);
+        }
+        lCursor.close();
+        lDb.close();
+        return lValue;
     }
     //===============================================
 }
