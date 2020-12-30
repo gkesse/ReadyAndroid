5ecd715a1749f881c6b733f3fefabf19076097cc
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-30 05:22:56 +0100

    Initial Commit

--- app/code/readyapp/app/src/main/java/com/pkg/readyapp/GAddressBar.java
+++ app/code/readyapp/app/src/main/java/com/pkg/readyapp/GAddressBar.java
@@ -22,7 +22,7 @@ public class GAddressBar extends GWidget implements OnClickListener,
         
         Button lIcon = new Button(context);
         lIcon.setText(lApp.icon_map.get("home"));
-        lIcon.setBackground(GAndroid.Instance().bgColor(lApp.button_bg, 0, 0, ""));
+        lIcon.setBackgroundColor(Color.TRANSPARENT);
         lIcon.setOnClickListener(this);
         lIcon.setTypeface(GManager.Instance().loadFace(lApp.icon_font));
         lIcon.setTransformationMethod(null);
@@ -37,32 +37,21 @@ public class GAddressBar extends GWidget implements OnClickListener,
         lAddress.setTypeface(GManager.Instance().loadFace(lApp.app_font));
         m_widgetMap.put(lAddress, "address");
         
-        GradientDrawable lGoToBg = new GradientDrawable();
-        lGoToBg.setColor(Color.parseColor(lApp.button_bg));
-        lGoToBg.setCornerRadius(lApp.button_border_radius);
-        lGoToBg.setStroke(lApp.button_border_width, Color.parseColor(lApp.button_border));
-
         Button lGoTo = new Button(context);
         lGoTo.setText(lApp.icon_map.get("paperplaneo"));
-        lGoTo.setBackground(lGoToBg);
+        lGoTo.setBackgroundColor(Color.TRANSPARENT);
         lGoTo.setOnClickListener(this);
         lGoTo.setTypeface(GManager.Instance().loadFace(lApp.icon_font));
         lGoTo.setTransformationMethod(null);
         lGoTo.setPadding(lApp.button_padding_lr, lApp.button_padding_tb, lApp.button_padding_lr, lApp.button_padding_tb);
         m_widgetMap.put(lGoTo, "goto");
-        
-        GradientDrawable lBgColor = new GradientDrawable();
-        lBgColor.setColor(Color.parseColor(lApp.button_bg));
-        lBgColor.setCornerRadius(lApp.button_border_radius);
-        lBgColor.setStroke(lApp.button_border_width, Color.parseColor(lApp.button_border));
 
-        setOrientation(LinearLayout.HORIZONTAL);       
         setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
         setPadding(5, 5, 5, 5);
         addView(lIcon, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
         addView(lAddress, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
         addView(lGoTo, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-        setBackground(lBgColor);
+        setBackground(GAndroid.Instance().bgColor(lApp.button_bg, 0, 0, ""));
     }
     //===============================================
     // callback
